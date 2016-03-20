dcad5aa053e6c4b2651a76eb8fccd91cea708266
guigui64 <guillaume.comte10@gmail.com>
2016-03-20 12:45:03 +0100

    code + vcs

--- main.cpp
+++ main.cpp
@@ -6,22 +6,24 @@
 using namespace std;
 
 int main() {
-    string empty{"not empty !"};
+    string empty = "not empty !"; // copy initialization
+    string empty2("not empty !"); // direct initialization
+    string empty3{"not empty !"}; // list initialization
     cout << "Hello, World! + \"" << empty << "\"" << endl;
+    cout << (empty == empty2) << " " << (empty == empty3) << endl;
 
     string sentence;
     cout << endl << "Enter some sentence and I will parse it !" << endl;
     getline(cin, sentence);
     cout << sentence << endl;
 
-//    istringstream iss{sentence};
-//    copy(istream_iterator<string>(iss),
-//         istream_iterator<string>(),
-//         ostream_iterator<string>(cout, "\n"));
-
-    const vector<string> splitWords{split(sentence, ' ')};
+    const vector<string> splitWords = split(sentence, ' ');
     for (auto i: splitWords)
         cout << i << endl;
+
+    for (auto &c : sentence)
+        c = (char) toupper(c);
+    cout << sentence << endl;
 
     return 0;
 }
